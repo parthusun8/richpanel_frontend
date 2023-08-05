a92b3e73bfd6987e5c12517acf80b233c3fc9caa
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-08-05 17:00:16 +0530

    hosting

--- src/Components/Axios/Axios.js
+++ src/Components/Axios/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL: "https://everidoorbackend.com/", //The API {Cloud Funtion}
-    baseURL : 'http://ec2-3-109-143-4.ap-south-1.compute.amazonaws.com:3000/'
+    baseURL : 'https://richpanel-backend.onrender.com/'
     // baseURL: "http://ec2-43-204-23-143.ap-south-1.compute.amazonaws.com:4000/"
 });
 

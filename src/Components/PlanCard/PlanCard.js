e92a0390a45231d21f77a3fb095ce51a33385824
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-08-06 18:33:39 +0530

    Traingle added in plans page

--- src/Components/PlanCard/PlanCard.js
+++ src/Components/PlanCard/PlanCard.js
@@ -6,7 +6,7 @@ function PlanCard({planName, id, setCurrentSelected, currentSelected}){
              <div className= {(id) === currentSelected ? "flex flex-col w-[6em] h-[6em] bg-box-blue-color items-center justify-center" : "flex flex-col w-[6em] h-[6em] bg-box-light-blue-color items-center justify-center opacity-50"} onClick={() => setCurrentSelected(id)}>
                 <div className="text-white text-xs">{planName}</div>
             </div>
-            {(id) === currentSelected ?<div class="w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-box-blue-color relative"></div> : <></>}
+            {(id) === currentSelected ? <div class="w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-box-blue-color relative"></div> : <></>}
          </div>
     )
 }

57a4ae8191d296081ed65fdeb262babc0160f1af
Parth Sundarka <77690791+parthusun8@users.noreply.github.com>
2023-08-06 18:30:27 +0530

    Bug Fixes

--- src/Components/PlanCard/PlanCard.js
+++ src/Components/PlanCard/PlanCard.js
@@ -4,8 +4,9 @@ function PlanCard({planName, id, setCurrentSelected, currentSelected}){
     return (
          <div className='flex flex-col items-center justify-center'>
              <div className= {(id) === currentSelected ? "flex flex-col w-[6em] h-[6em] bg-box-blue-color items-center justify-center" : "flex flex-col w-[6em] h-[6em] bg-box-light-blue-color items-center justify-center opacity-50"} onClick={() => setCurrentSelected(id)}>
-                <div className="text-white text-xs">{planName}</div> 
+                <div className="text-white text-xs">{planName}</div>
             </div>
+            {(id) === currentSelected ?<div class="w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-box-blue-color relative"></div> : <></>}
          </div>
     )
 }
